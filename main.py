3013fb0c0162aac19834aa3cd09d2e3a29dbcc7d
David Snyder <davidgraygs4@gmail.com>
2019-11-30 09:57:18 -0500

    added more tiles

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ tile_size = input("Enter Tile Size: ")
 size_reduction = int(input("Enter image size reduction scale: "))
 new = reduce_size(im, size_reduction)
 
-URL = PATH+f"{tile_size}x/"
+
+URL = PATH+f"{tile_type}/{tile_size}x/"
+if not os.path.isdir(URL):
+    URL = PATH+f"{tile_type}/"
 paths = os.listdir(URL)
 paths = [p for p in paths if p.endswith(".png")]
 
@@ -25,8 +28,10 @@ alpha = imread(URL+paths[0]).shape[2]
 print("Loading Tiles")
 for path in tqdm(paths):
     img = imread(URL+path)
-    if img.shape[1] == int(tile_size) and img.shape[0] ==int(tile_size) and img.shape[2] == alpha:
+    if img.shape[2] == alpha:
+        img = cv2.resize(img, dsize=(int(tile_size), int(tile_size)), interpolation=cv2.INTER_CUBIC)
         images.append(img)
+
 
 colors = []
 print()
@@ -55,13 +60,13 @@ try:
     end = stitch(final, (m, r), (int(tile_size), int(tile_size)))
 
     filename = input("Enter output filename: ")
-    imsave(filename, end)
-    
+    imsave("output/"+filename, end)
+
     plt.imshow(end)
     plt.show()
-except:
+except Exception as e:
     final = None
-    print("Memory Error")
+    print(e)
 
 
 
