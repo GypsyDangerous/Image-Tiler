682e1b96f3f2acbc438b35f4db8702efbb1f3b73
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-12-02 15:56:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ while True:
     try:
         im = imread(PATH+picture_name, 0)
     except:
+        print(f"{PATH+picture_name} is an Invalid image path")
         continue
     break
 
 tile_type = input("Enter Tile Type: ")
-tile_size = input("Enter Tile Size: ")
+disclaimer = ", Minecraft tiles only support '8' and '16' tile sizes" if tile_type == "minecraft" else ""
+tile_size = input(f"Enter Tile Size{disclaimer}: ")
 size_reduction = int(input("Enter image size reduction scale: "))
 new = reduce_size(im, size_reduction)
 
 
 URL = PATH+f"{tile_type}/{tile_size}x/"
 if not os.path.isdir(URL):
+    if tile_type == "minecraft":
+        raise Exception("Invalid tile size for minecraft tiles")
     URL = PATH+f"{tile_type}/"
 paths = os.listdir(URL)
 paths = [p for p in paths if p.endswith(".png")]
@@ -25,8 +29,7 @@ paths = [p for p in paths if p.endswith(".png")]
 images = []
 alpha = imread(URL+paths[0]).shape[2]
 
-print("Loading Tiles")
-for path in tqdm(paths):
+for path in tqdm(paths, desc = "Loading Tiles"):
     img = imread(URL+path)
     if img.shape[2] == alpha:
         img = cv2.resize(img, dsize=(int(tile_size), int(tile_size)), interpolation=cv2.INTER_CUBIC)
@@ -34,16 +37,12 @@ for path in tqdm(paths):
 
 
 colors = []
-print()
-print("getting colors from tiles")
-for i, img in tqdm(enumerate(images)):
+for i, img in (enumerate(images)):
     colors.append((i, get_average_color(img)))
 
 final = []
 m, r = new.shape[:2]
-print()
-print("generating image")
-for i in tqdm(range(m)):
+for i in tqdm(range(m), desc="generating Image"):
     for j in range(r):
         index = 0
         min_dist = math.inf
